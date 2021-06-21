828056183ff2ca7928a7fb8cddaf561c2aa20815
Leonardo Antonio Nolasco Leyva <leo@pop-os.localdomain>
2021-06-21 12:24:42 -0500

    home - profile

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,23 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <HeaderComponent/>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
+<script>
+import HeaderComponent from './components/header.component'
+export default {
+  name: "App",
+  components: {
+    HeaderComponent,
+  },
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.v-application--wrap {
+  background: radial-gradient(33.06% 65.37% at 100% 100%, rgba(255, 255, 255, 0.2) 0%, rgba(0, 0, 0, 0) 100%), radial-gradient(39.72% 78.55% at 4.79% 7.96%, rgba(255, 255, 255, 0.2) 0%, rgba(7, 33, 66, 0.094) 100%), #072142;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,82 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container class="">
+    <div>
+      <div>
+        <div class="py-10">
+          <div>
+            <h1 class="saira-semibold text-center text-primary">
+              Leonardo Antonio
+              <span class="text-secundary">
+                Nolasco Leyva
+              </span>
+            </h1>
+            <span class="text-center subtitle">
+              <h4>DESARROLLADOR DE SOFTWARE</h4>
+            </span>
+          </div>
+        </div>
+
+        <v-row>
+          <v-col align="end">
+            <div>
+              <img src="../assets/img__main__home.svg" width="420px" alt="" />
+            </div>
+          </v-col>
+          <v-col align="start">
+            <div class="w50">
+              <div class="text-md text-white pb-5 pt-10">
+                Desarrollador Backend con concimientos en el Frontend
+              </div>
+              <div>
+                <button class="text-white btn">
+                  Contáctame
+                </button>
+              </div>
+            </div>
+          </v-col>
+        </v-row>
+      </div>
+    </div>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+};
 </script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Saira:wght@600&display=swap");
+
+.text-secundary {
+  color: #00aae4;
+}
+.text-primary {
+  color: #fff;
+}
+.saira-semibold {
+  font-family: Saira;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 70px;
+  line-height: 52px;
+}
+
+.subtitle {
+  font-family: Saira;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 25px;
+  line-height: 55px;
+
+  color: #ffffff;
+}
+
+.btn {
+  background: #00aae4;
+  border-radius: 50px;
+  width: 100%;
+  padding: 0.5rem;
+}
+</style>
